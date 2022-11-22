d4d26d4a788af654dd8c43f720075556cf901a7b
Anatolii Pismennyi <105397905+Giperion317@users.noreply.github.com>
2022-11-22 01:54:26 +0100

    Uploads task-01 ProtoTypes Styles

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,30 +1,47 @@
-export const Profile = ({ user: { username, tag, location, avatar, stats:{followers,views,likes}} }) => {
+import PropTypes from 'prop-types';
+import styles from './Profile.module.css';
+
+export const Profile = ({ user: { username, tag, location, avatar, stats: { followers, views, likes } } }) => {
     
-  return <div className="profile">
-  <div className="description">
+  return <div className={styles.profile}>
+  <div className={styles.description}>
     <img
       src={avatar}
       alt="username"
-      className="avatar"
+      className={styles.avatar}
     />
-    <p className="name">{username}</p>
-    <p className="tag">{tag}</p>
-      <p className="location">{location}</p>
+    <p className={styles.name}>{username}</p>
+    <p className={styles.tag}>{tag}</p>
+      <p className={styles.location}>{location}</p>
   </div>
 
-  <ul className="stats">
+  <ul className={styles.stats}>
     <li>
-      <span className="label">Followers</span>
-        <span className="quantity">{followers}</span>
+      <span className={styles.lable}>Followers</span>
+        <span className={styles.quantity}>{followers}</span>
     </li>
     <li>
-      <span className="label">Views</span>
-      <span className="quantity">{views}</span>
+      <span className={styles.lable}>Views</span>
+      <span className={styles.quantity}>{views}</span>
     </li>
     <li>
-      <span className="label">Likes</span>
-      <span className="quantity">{likes}</span>
+      <span className={styles.lable}>Likes</span>
+      <span className={styles.quantity}>{likes}</span>
     </li>
   </ul>
 </div>;
 };
+
+Profile.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    }),
+  }),
+}
