0a601a655b7a4bb598e3addd53f4974036432051
Anatolii Pismennyi <105397905+Giperion317@users.noreply.github.com>
2022-11-23 03:53:23 +0100

    Uploads task-02 Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import { Profile } from "./Profile/Profile";
+import { Statistics } from './Statistics/Statistics'
 import user from "../data/user";
+import data from "../data/data";
 
 export const App = () => {
   return (
-    <div>
+    <>
       <Profile user={user} />
-    </div>
+      <Statistics stats={data} title="Upload stats"/>
+    </>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+import styles from './Statistics.module.css';
+import {Stats} from './Stats'
+
+export const Statistics = ({stats, title}) => {
+    return <section className={styles.statistics}>
+    {title !==''&&<h2 className={styles.title}>{title}</h2>}
+    <Stats stats={stats}/>
+    </section>
+}
+
+Statistics.propTypes = {
+    stats: PropTypes.array.isRequired,
+    title: PropTypes.string,
+}
